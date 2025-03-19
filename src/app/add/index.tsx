c4dc1fcb6387d42d7906b88b2b1dc7dfa88dfe73
Fernando Fantinel <fantinel89+github@gmail.com>
2025-03-19 20:42:02 -0300

    app done

---
+++ src/app/add/index.tsx
@@ -0,0 +1,75 @@
+import { View, Text, TouchableOpacity, Alert } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
+import { router } from "expo-router"
+import { useState } from "react"
+
+import { styles } from "./styles"
+import { colors } from "@/styles/colors"
+import { linkStorage } from "@/storage/link-storage"
+
+import { Categories } from "@/components/categories"
+import { Input } from "@/components/input"
+import { Button } from "@/components/button"
+
+export default function Add() {
+  const [category, setCategory] = useState("")
+  const [name, setName] = useState("")
+  const [url, setUrl] = useState("")
+
+  async function handleAdd() {
+    try {
+      if (!category) {
+        return Alert.alert("Categoria", "Selecione a categoria.")
+      }
+
+      if (!name.trim()) {
+        return Alert.alert("Nome", "Selecione o nome.")
+      }
+
+      if (!url.trim()) {
+        return Alert.alert("URL", "Selecione a URL.")
+      }
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possível salvar o link.")
+    }
+
+    await linkStorage.save({
+      id: new Date().getTime().toString(),
+      name,
+      url,
+      category,
+    })
+
+    Alert.alert("Sucesso", "Novo link adicionado.", [
+      {
+        text: "Ok",
+        onPress: () => router.back(),
+      },
+    ])
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => router.back()}>
+          <MaterialIcons name="arrow-back" size={32} color={colors.gray[200]} />
+        </TouchableOpacity>
+        <Text style={styles.title}>Novo</Text>
+      </View>
+
+      <Text style={styles.label}>Selecione uma categoria</Text>
+      <Categories onChange={setCategory} selected={category} />
+
+      <View style={styles.form}>
+        <Input
+          placeholder="Nome"
+          onChangeText={setName}
+          autoCorrect={false}
+          autoCapitalize="none"
+        />
+        <Input placeholder="URL" onChangeText={setUrl} autoCorrect={false} />
+        <Button title="Adicionar" onPress={handleAdd} />
+      </View>
+    </View>
+  )
+}
