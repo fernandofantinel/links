c1133a79ac459ab36861cb0c0b177a06c0e2fffc
Fernando Fantinel <fantinel89+github@gmail.com>
2025-03-20 06:11:59 -0300

    change paddingTop value in `index` and `add` screens

--- src/app/add/styles.ts
+++ src/app/add/styles.ts
@@ -4,7 +4,7 @@ import { StyleSheet } from "react-native"
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 62,
+    paddingTop: 32,
   },
   header: {
     flexDirection: "row",
